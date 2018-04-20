378f4bb5a6d67d65c599ab4f7fbf7f8b5d193a76
viethoc <viethocpro@gmail.com>
2018-04-21 06:54:15 +0700

    pass test report

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do 
    scope '/api' do
-    mount_devise_token_auth_for 'User', at: 'auth'
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'user/registrations',
+      sessions: 'user/sessions'
+    }
     resources :reports
   end
 end
