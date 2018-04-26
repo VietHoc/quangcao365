58046fbd48a65702646f9e0fb52659f3523c29dd
viethoc <viethocpro@gmail.com>
2018-04-26 12:14:57 +0700

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423014637) do
+ActiveRecord::Schema.define(version: 20180425111209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.string "content"
+    t.string "product"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "cate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/articles.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :article do
+    title "MyString"
+    image "MyString"
+    content "MyString"
+    product "MyString"
+  end
+end
