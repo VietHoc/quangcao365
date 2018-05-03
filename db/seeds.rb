f5a64197f6a465ee08810fee66d727779da1a1d1
viethoc <viethocpro@gmail.com>
2018-05-03 18:00:21 +0700

    edit database

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,3 @@ Report.create(
     email: "tuannho@novahub.vn",
     description: Faker::Lorem
 )
-
-product=["In Kính", "In Mika", "In Formex", "In Alumminium", "In Gỗ",
-        "Chữ Nổi Mica", "Chữ Nổi Aluminium", "Chữ Nỗi Inox",
-        "Tranh Uv", "Tranh Trang Tri",
-        "Biển Aluminium", "Biển Bạt Hiflex", "bien_bat_3m", "bien_mica", "bien_treo_tran", "bien_vay", "drop_quang_cao",
-        "bien_inox_an_mon", "bien_chuc_danh",
-        "hop_den_sieu_mong", "hop_den_uv", "hop_den_led",
-        "decal_mo", "decal_pp", "decal_oto", "decal_trong", "decal_trang_tri",
-        "nguon_den", "standee"]
-product.length.times do |n|
-    Product.create(name: product[n])
-end
